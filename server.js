d1e375672a5d5f822e38541e5ced66c72f5170a1
Jeevendra Kumar <rxjeeva@gmail..com>
2020-09-27 19:06:48 -0400

    committing sql-3

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connecttionString : process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    },
+    ssl: true,
   }
 });
 
